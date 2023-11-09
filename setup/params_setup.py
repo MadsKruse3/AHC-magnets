04ade12d5a394505e5f1790d578d44f91acc19a2
madkru <madkru@dtu.dk>
2023-11-09 11:49:27 +0100

    Set calculation parameters for groundstate.

---
+++ setup/params_setup.py
@@ -0,0 +1,9 @@
+"""Submit the following to the each folder:
+
+asr run "setup.params asr.gs@calculate:calculator {'kpts':{'density':33,'gamma':True},'xc':'PBE','maxiter':5000,'mixer':{'method':'difference','beta':0.02,'history':3,'weight':100},...}"
+
+"""
+
+
+
+
